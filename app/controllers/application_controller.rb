d9557eebcdbb17665c6e2ceea687671ed487231c
“Mark <mkanko66@gmail.com>
2019-08-30 17:26:51 -0700

    write current_user and logged_in helper methods in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in? 
+
+    def current_user
+        if session[:user_id]
+            @current_user = User.find_by_id(session[:user_id])
+        end 
+    end 
+
+    def logged_in?
+        !!current_user 
+    end 
+
 end
+
