0c1d0dfe2d5077ce5465be6715f0d63f0b49c413
“Mark <mkanko66@gmail.com>
2019-08-30 17:16:42 -0700

    add new, create, and show actions to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+    def new 
+        @user = User.new 
+    end 
+
+    def create
+        @user = User.new(user_params)
+
+        return redirect_to new_user_path unless @user.save 
+        session[:user_id] = @user.user_id
+
+        redirect_to user_path(@user)
+    end
+    
+    def show
+        if !logged_in?
+            redirect_to '/'
+        else
+            @user = User.find(params[:id])
+        end
+    end 
+    
 end
