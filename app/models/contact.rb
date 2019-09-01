4c8193b8310846f9f9b1f1ea7d8e91f93ab78bd4
“Mark <mkanko66@gmail.com>
2019-08-31 20:03:44 -0700

    add custom setter and getter methods to contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,12 @@
 class Contact < ApplicationRecord
     belongs_to :user 
+
+    def user_name=(name)
+        self.user = User.find_or_create_by(name: name)
+    end
+    
+    def user_name
+        self.user ? self.user.name : nil
+    end
 end
+
