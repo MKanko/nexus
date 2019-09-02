b5649a3d7fdcf8c339c7118b838bea32e990ece0
“Mark <mkanko66@gmail.com>
2019-09-02 11:07:20 -0700

    add index, new, create actions to clients_controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,16 @@
 class ClientsController < ApplicationController
+
+    def index
+        @clients = Client.all 
+    end 
+
+    def new 
+        @client = Client.new 
+    end 
+
+    def create 
+        @client = Client.new(client_params)
+        return redirect_to new_client_path unless @client.save
+        redirect_to client_path(@client)
+    end
 end
