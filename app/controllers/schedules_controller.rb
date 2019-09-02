b8d1ff8bdf82ba750aa1c356ba40af62bdfec900
“Mark <mkanko66@gmail.com>
2019-09-02 14:27:34 -0700

    add index, new, create actions to schedules_controller.rb

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,16 @@
 class SchedulesController < ApplicationController
+
+    def index
+        @schedules = Schedule.all 
+    end 
+
+    def new
+        @schedule = Schedule.new 
+    end 
+
+    def create
+        @schedule = Schedule.new(schedule_params)
+        return redirect_to new_schedule_path unless @schedule.save
+        redirect_to schedule_path(@schedule)
+    end 
 end
