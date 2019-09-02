fb98d0563b5de2fb3e11fc60ed9fd9dcae11230a
“Mark <mkanko66@gmail.com>
2019-09-02 11:58:27 -0700

    add associative setter/getter methods to client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,12 @@
 class Client < ApplicationRecord
   belongs_to :contact
+
+  def contact_company_name=(name)
+    self.contact = Contact.find_or_create_by(name: name)
+  end
+
+  def user_username
+      self.contact ? self.contact.name : nil
+  end
+
 end
