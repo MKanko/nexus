bab87316cd344a916be5d67a571976636346308e
“Mark <mkanko66@gmail.com>
2019-09-02 16:39:02 -0700

    write associative setter/getter methods in schedule.rb, add permission to strong params in schedules_controller.rb

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,11 @@
 class Schedule < ApplicationRecord
   belongs_to :user
+
+  def user_username=(name)
+    self.user = User.find_or_create_by(username: name)
+  end 
+
+  def user_username
+    self.user ? self.user.username : nil
+  end  
 end
