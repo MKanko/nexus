5b2c62b1e41fe49ef3c70e5043e62f7289040309
“Mark <mkanko66@gmail.com>
2019-09-02 18:38:12 -0700

    write show, edit, update, destroy actions in tasks_controller.rb and strong params method in tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,12 +5,37 @@ class TasksController < ApplicationController
     end 
 
     def new 
-        @task = Task.new 
+        @task = Task.new
     end 
 
     def create 
         @task = Task.new(task_params)
-        return redirect_to new_task_path unless @task.save
-        redirect_to task_path(@path)
+        return redirect_to new_task_params unless @task.save
+        redirect_to task_path(@task)
     end 
+
+    def show
+        @task = Task.find(params[:id])
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(name: params[:task][:name], type: params[:task][:type], status: params[:task][:status], deadline: params[:task][:deadline], notes: params[:task][:notes])
+        redirect_to task_path(@task)
+    end 
+
+    def destroy
+        @task = Task.find(params[:id]).destroy
+    end 
+
+    private
+
+    def task_params
+        params.require(:task).permit(:name, :type, :status, :deadline, :notes)
+    end 
+        
 end
